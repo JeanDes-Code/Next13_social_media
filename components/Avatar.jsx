e9c51afd0dd7c8dd75fa4a979284c09d6bf9efcf
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-12-20 12:55:19 +0100

    init : Social media Next13 (without appDir 4now)

---
+++ components/Avatar.jsx
@@ -0,0 +1,12 @@
+const Avatar = () => {
+    return (
+        <div className="w-12 rounded-full overflow-hidden">
+            <img
+                src="https://images.unsplash.com/photo-1611367540679-d94566094025?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                alt="avatar"
+            />
+        </div>
+    )
+}
+
+export default Avatar
---
+++ components/Card.jsx
@@ -0,0 +1,7 @@
+const Card = ({children}) => {
+  return (
+    <div className='bg-white shadow-gray-300 shadow-md rounded-md p-4 mb-5'>{children}</div>
+  )
+}
+
+export default Card
